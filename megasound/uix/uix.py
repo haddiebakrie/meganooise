1b7781587f37ed783fe8daf174895d6c34f8ba4b
Haddie Bakrie <76763808+cutehaddy@users.noreply.github.com>
2021-08-21 03:09:25 +0100

    Delete megasound directory

--- megasound/uix/uix.py
+++
@@ -1,7 +0,0 @@
-from uix.topbar import TopBarMenu, TopBarBack, TopBarSelected
-from uix.searchfield import SearchFieldHalf, SearchField
-from uix.button import FAB, IconButton, ButtonBottom
-from uix.musiccard import MusicCard, MusicCardPlaylist
-from uix.bottomplayer import BottomPlayer
-from uix.playlistcard import PlayListCard
-from uix.navdrawer import NavDrawer, NavListItem
